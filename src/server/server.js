1246ec66fa921e4cf58b50638ed182374b383e25
thomas.adriano <thomas.adriano@senior.com.br>
2016-11-18 14:08:14 -0200

    Implementada persistencia de partidas

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const qrCode = require('qrcode-npm')
+const qrCode = require('qrcode-npm');
+const repo = require('./repository');
+var bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json()); // for parsing application/json
+
+app.get('/status', (req, res) => {
+    res.json({
+        status: 'OK'
+    });
+});
 
 app.get('/generate-qrcode', (req, res) => {
     if (!req.query || !req.query.data) {
@@ -37,6 +46,49 @@ app.get('/generate-qrcode', (req, res) => {
             data: d
         });
     }
+});
+
+app.post('/match/:matchId', (req, res) => {
+    let matchId = req.params.matchId;
+    let data = req.body;
+    let errorMsg = '';
+
+    if (!matchId) {
+        errorMsg += 'um codigo de partida deve ser informado; '
+    }
+
+    if (!data) {
+        errorMsg += 'um objeto representando a partida deve ser enviado no corpo do post; '
+    }
+
+    if (errorMsg) {
+        res.status(400).json({
+            msg: errorMsg
+        });
+        return;
+    }
+
+    repo.put(matchId, data)
+        .then(r => res.json(r))
+        .catch(e => {
+            res.status(500).json(e);
+        });
+});
+
+app.get('/match/:matchId', (req, res) => {
+    let matchId = req.params.matchId;
+    if (!matchId) {
+        res.status(400).json({
+            msg: 'um codigo de partida deve ser informado.'
+        });
+        return;
+    }
+
+    repo.get(matchId)
+        .then(r => res.json(r))
+        .catch(e => {
+            res.status(500).json(e);
+        });
 });
 
 const clientPath = path.resolve(path.resolve(__dirname, '..'), '../dist');
